3d614815c27ab759b62ce7416ba7ae7c96422f72
EpocDotFr <contact@epoc.fr>
2024-04-05 11:30:12 +0200

    Fix build stage

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ REQUIRED = [
 
 EXTRAS = {
     'dev': {
-        'wheel~=0.43',
+        'build~=1.2',
         'twine~=5.0',
     }
 }
@@ -88,8 +88,8 @@ class UploadCommand(Command):
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status('Building Source and Wheel distribution…')
+        os.system('"{0}" -m build --sdist --wheel'.format(sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
         os.system('twine upload dist/*')
